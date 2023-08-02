a15641a8093701a4a3cd98f8919700b57d7212df
hemant14050 <hemant14050@gmail.com>
2023-08-02 08:59:09 +0530

    created pre-middleware in OTP model

--- backend/models/OTP.js
+++ backend/models/OTP.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const mailSender = require("../utils/mailSender");
 
 const OTPSchema = new mongoose.Schema({
     email: {
@@ -14,6 +15,24 @@ const OTPSchema = new mongoose.Schema({
         default: Date.now(),
         expires: 5*60, // expires in five minutes
     }
+});
+
+// function which sends emails for otp
+async function sendVerificationEmail(email, otp) {
+    try {
+        const mailResponse = await mailSender(email, "Verification Email from StudyNotion", otp);
+        console.log("Email sent Successfully!", mailResponse);
+        
+    } catch(err) {
+        // error occured while sending-email
+        console.log("Error while sending verification mail!", err);
+        throw err;
+    }
+}
+
+OTPSchema.pre("save", async function(next){
+    await sendVerificationEmail(this.email, this.otp);
+    next();
 });
 
 module.exports = mongoose.model("OTP", OTPSchema);
