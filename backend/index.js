2f8c63fe0d640ffa73363a2fd71aeda3d4d178a0
hemant14050 <hemant14050@gmail.com>
2023-12-19 10:51:38 +0530

    feat: added payment integration using razorpay

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const app = express();
 const userRoutes = require("./routes/User");
 const courseRoutes = require("./routes/Course");
 const profileRoutes = require("./routes/Profile");
+const paymentRoutes = require("./routes/Payments");
 
 const db = require("./config/database");
 const cookieParser = require("cookie-parser");
@@ -40,6 +41,7 @@ connectCloudinary();
 app.use("/api/v1/auth", userRoutes);
 app.use("/api/v1/course", courseRoutes);
 app.use("/api/v1/profile", profileRoutes);
+app.use("/api/v1/payment", paymentRoutes);
 
 // TEST MAIL
 // const mailSender = require("./utils/mailSender");
